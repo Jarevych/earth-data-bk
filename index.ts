4b6b687eb485f2f11dffea027c9ffddbd63f7baf
Yaroslav Grabovych <ygrabovych@gmail.com>
2024-03-24 23:13:28 +0200

    first commit
    fix

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('welcome')
+});
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
